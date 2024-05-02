a5b06391c3effa0d97a925dd7245fd2669530158
wallaceew <165271215+wallaceew@users.noreply.github.com>
2024-05-02 21:40:35 +0100

    defining legend and scale bar for map output

--- green_assignment.py
+++ green_assignment.py
@@ -16,3 +16,28 @@ import matplotlib.lines as mlines
 # make plotting interactive 
 plt.ion() 
 
+# generate matplotlib handles to create a legend of each of the features we put in our map.
+def generate_handles(labels, colors, edge='k', alpha=1):
+    lc = len(colors)  # get the length of the color list
+    handles = [] # create an empty list
+    for ii in range(len(labels)): # for each label and color pair that we're given, make an empty box to pass to our legend
+        handles.append(mpatches.Rectangle((0, 0), 1, 1, facecolor=colors[ii % lc], edgecolor=edge, alpha=alpha))
+    return handles
+
+# create a scale bar of length 20 km in the upper right corner of the map
+# adapted this question: https://stackoverflow.com/q/32333870
+# answered by SO user Siyh: https://stackoverflow.com/a/35705477
+def scale_bar(ax, location=(0.92, 0.95)):
+    x0, x1, y0, y1 = ax.get_extent() # get the current extent of the axis
+    sbx = x0 + (x1 - x0) * location[0] # get the lower left x coordinate of the scale bar
+    sby = y0 + (y1 - y0) * location[1] # get the lower left y coordinate of the scale bar
+
+    ax.plot([sbx, sbx - 20000], [sby, sby], color='k', linewidth=9, transform=ax.projection) # plot a thick black line, 20 km long
+    ax.plot([sbx, sbx - 10000], [sby, sby], color='k', linewidth=6, transform=ax.projection) # plot a smaller black line from 0 to 10 km long
+    ax.plot([sbx-10000, sbx - 20000], [sby, sby], color='w', linewidth=6, transform=ax.projection) # plot a white line from 10 to 20 km
+
+    ax.text(sbx, sby-5000, '20 km', transform=ax.projection, fontsize=8) # add a label at 20 km
+    ax.text(sbx-12500, sby-5000, '10 km', transform=ax.projection, fontsize=8) # add a label at 10 km
+    ax.text(sbx-24500, sby-5000, '0 km', transform=ax.projection, fontsize=8) # add a label at 0 km
+
+    return ax
