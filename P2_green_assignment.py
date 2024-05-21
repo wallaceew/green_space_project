594a68e768c9160365988c7e52bf9786aa668912
wallaceew <165271215+wallaceew@users.noreply.github.com>
2024-05-21 22:03:36 +0100

    Adding the wards shapefile

--- P2_green_assignment.py
+++ P2_green_assignment.py
@@ -8,3 +8,6 @@ import numpy as np
 import matplotlib.pyplot as plt  # For plotting data
 from pyproj import Proj, transform  # For transforming coordinate systems
 from osgeo import gdal  # For handling and converting geospatial data formats
+
+# Load ward boundaries shapefile and convert to WGS84 coordinate reference system (CRS)
+wards = gpd.read_file('C:/EGM722/egm722/green_space_project/data_files/wardsPolygon.shp').to_crs(epsg=4326)
